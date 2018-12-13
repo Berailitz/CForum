10dd51f086491b992827c798b7bf1c2b3478d44f
Berailitz <admin@ohhere.xyz>
2018-12-13 12:44:24 +0800

    Add CForum, use pointer, add ~ForumServer;

--- forum_server.h
+++ forum_server.h
@@ -7,28 +7,34 @@
 #include <QWebSocket>
 #include <QWebSocketServer>
 
+#include "cforum.h"
+#include "request_message.h"
+
 namespace cforum
 {
     class ForumServer : public QObject
     {
         Q_OBJECT
     public:
-        ForumServer();
+		ForumServer();
+		~ForumServer();
         bool listen(const int port = 8118);
 
     public Q_SLOTS:
         void onNewConnection();
-        void onTextMessageReceived(const QString &message);
+        void onTextMessageReceived(const QString &textMessage);
         void onDisconnection();
-        void sendMessage(const QString &target, const QString &message);
+        void sendMessage(const QString &target, const QString &textMessage);
 
     Q_SIGNALS:
         void messageReceived(const QString &message);
 
     private:
+		CForum *cforum;
         QWebSocketServer *server;
-        QList<QWebSocket *> clients;
+        QList<QWebSocket *> *clients;
         QString hashSocket(const QWebSocket &socket);
+		void execute(const RequestMessage &message);
     };
 }
 
