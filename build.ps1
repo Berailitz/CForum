57683f47326237ddbc52c38a491cefbac3333ee1
Berailitz <admin@ohhere.xyz>
2018-12-16 16:59:45 +0800

    Use Creator's build for VS build has no Qt.labs.platform release;

--- build.ps1
+++ build.ps1
@@ -1,2 +1,6 @@
-Copy-Item -Force -Recurse data release/
-windeployqt  release/CForum.exe
+Remove-Item -Force -Recurse "build/"
+Copy-Item -Force -Recurse "./data" "build/data"
+Copy-Item -Force "../build-CForum-Desktop_Qt_5_11_2_MSVC2017_64bit-Release/release/*.exe" "build/"
+Get-ChildItem -Path "build/*.exe" | ForEach-Object {
+    windeployqt --no-patchqt $_.FullName
+}
