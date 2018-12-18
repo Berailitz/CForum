04ad14a571e1b0f7e90487060377811095759954
Berailitz <admin@ohhere.xyz>
2018-12-18 14:36:51 +0800

    Fix build.ps1: include qml;

--- build.ps1
+++ build.ps1
@@ -2,5 +2,5 @@ Remove-Item -Force -Recurse "build/"
 Copy-Item -Force -Recurse "./data" "build/data"
 Copy-Item -Force "../build-CForum-Desktop_Qt_5_11_2_MSVC2017_64bit-Release/release/*.exe" "build/"
 Get-ChildItem -Path "build/*.exe" | ForEach-Object {
-    windeployqt --no-patchqt $_.FullName
+    windeployqt --qmldir "./qml" $_.FullName
 }
