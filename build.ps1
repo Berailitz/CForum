53d83d3140b247ac614a9bf840d8a8c6aef06db9
Berailitz <admin@ohhere.xyz>
2018-12-23 19:46:36 +0800

    Fix build.ps1;

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 Remove-Item -Force -Recurse "build/"
 Copy-Item -Force -Recurse "./data" "build/data"
-Copy-Item -Force "../build-CForum-Desktop_Qt_5_11_2_MSVC2017_64bit-Release/release/*.exe" "build/"
+Copy-Item -Force "../build-CForumServer-Desktop_Qt_5_11_2_MSVC2017_64bit-Release/release/*.exe" "build/"
+Copy-Item -Force "../build-CForumClient-Desktop_Qt_5_11_2_MSVC2017_64bit-Release/release/*.exe" "build/"
 Get-ChildItem -Path "build/*.exe" | ForEach-Object {
     windeployqt --qmldir "./qml" $_.FullName
 }
